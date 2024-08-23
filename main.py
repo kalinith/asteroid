119425d4e356ae3f6db00a6805e4c572ec6b8769
Neil van Vuuren <kalinith@gmail.com>
2024-08-23 13:25:47 +0200

    set refresh rate for game

--- main.py
+++ main.py
@@ -5,14 +5,16 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    ticker = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen,(0,0,0))
         pygame.display.flip()
-
+        dt = ticker.tick(60) / 1000
 
 
 if __name__ == "__main__":
