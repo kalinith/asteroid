14ac5f38d5cacfb7521ec1a4d1591c57ae1bc799
Neil van Vuuren <kalinith@gmail.com>
2024-08-23 13:59:43 +0200

    player 1 has arrived

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import Player
 
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
@@ -7,12 +8,16 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     ticker = pygame.time.Clock()
     dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen,(0,0,0))
+        player.draw(screen)
         pygame.display.flip()
         dt = ticker.tick(60) / 1000
 
